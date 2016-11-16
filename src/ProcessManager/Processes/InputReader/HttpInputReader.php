c953f7251f0bc0f8be66cf7c1a074f06218753b7
João Pedro <joaopedrodslv@gmail.com>
2016-11-16 04:33:32 -0200

    http input reader super global updated

--- src/ProcessManager/Processes/InputReader/HttpInputReader.php
+++ src/ProcessManager/Processes/InputReader/HttpInputReader.php
@@ -18,11 +18,11 @@ class HttpInputReader extends InputReader
 	public function __construct($args) 
 	{
 		parent::__construct($args);
-		$route = $_SERVER['REDIRECT_URL'];
+		$route = $_SERVER['REDIRECT_URL'] ?? $_SERVER['REQUEST_URI'];
 		$this->query = $_SERVER['QUERY_STRING'];
 		$this->method = $_SERVER['REQUEST_METHOD'];
 		$this->request = $args['_'.$this->method];
-		$test = str_replace('/', '\/', BASE_URI);
+		$test = str_replace('/', '\/', rtrim(BASE_URI, '/') . '/');
 		preg_match('/\/' . $test . '(.*?)\/?$/', $route, $matches);
 		if (!isset($matches[1])) {
 			throw new \Exception("Sua configuração para BASE_URI não pode ser encontrada na url.", 1);
